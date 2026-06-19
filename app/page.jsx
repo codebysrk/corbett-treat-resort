1e02c76bbbf798f0fc82ae37296cd3ed6c9e11b1
codebysrk <maishahrukhh@gmail.com>
2026-06-19 17:06:56 +0530

    chore: comment out ActivitiesSection on home page

--- app/page.jsx
+++ app/page.jsx
@@ -22,7 +22,7 @@ export default function Home() {
         <RoomsShowcase />
         {/* <DiningSection /> */}
         <Amenities />
-        <ActivitiesSection />
+        {/* <ActivitiesSection /> */}
         <GallerySection />
         {/* <PlacesToVisit /> */}
       </main>
